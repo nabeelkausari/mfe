69cb8c07a41ac889058237e5682cb8895e2abdd5
nk0005 <nabeel.kausari@analyttica.com>
2022-07-07 12:19:19 +0530

    update

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp"
 export default () => {
   return (
     <div>
-      <h1>Hi there from container!</h1>
+      <h1>Hi there from container updated!</h1>
       <hr/>
       <MarketingApp />
     </div>
